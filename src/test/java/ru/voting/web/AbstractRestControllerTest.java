10abc9dfe1a063d77baed459837c8981f9ef5f5f
kate <kate.krivikh@gmail.com>
2020-09-02 05:49:25 +0300

    UTF-8 is supported.

--- src/test/java/ru/voting/web/AbstractRestControllerTest.java
+++ src/test/java/ru/voting/web/AbstractRestControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import ru.voting.util.exception.ErrorType;
 
 import javax.annotation.PostConstruct;
@@ -23,6 +24,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 })
 @Transactional
 public class AbstractRestControllerTest {
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
 
     private MockMvc mockMvc;
 
@@ -33,6 +40,7 @@ public class AbstractRestControllerTest {
     private void postConstruct() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
+                .addFilter(CHARACTER_ENCODING_FILTER)
                 .apply(springSecurity())
                 .build();
     }
