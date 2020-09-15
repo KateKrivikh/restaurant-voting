427ed53f843eeae805fa87dbfebe35663c7b3328
kate <kate.krivikh@gmail.com>
2020-09-15 07:09:30 +0300

    DishRepository is removed.

--- src/main/java/ru/graduation/voting/service/DishService.java
+++ src/main/java/ru/graduation/voting/service/DishService.java
@@ -6,9 +6,11 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import ru.graduation.voting.model.Dish;
-import ru.graduation.voting.repository.DishRepository;
+import ru.graduation.voting.repository.CrudDishRepository;
+import ru.graduation.voting.repository.CrudRestaurantRepository;
 import ru.graduation.voting.to.MenuTo;
 
 import java.time.LocalDate;
@@ -23,48 +25,56 @@ import static ru.graduation.voting.util.ValidationUtil.*;
 public class DishService {
     private final Logger log = getLogger(DishService.class);
 
-    private final DishRepository repository;
+    private final CrudDishRepository repository;
+    private final CrudRestaurantRepository restaurantRepository;
 
-    public DishService(DishRepository repository) {
+    public DishService(CrudDishRepository repository, CrudRestaurantRepository restaurantRepository) {
         this.repository = repository;
+        this.restaurantRepository = restaurantRepository;
     }
 
     public List<Dish> getAll(int restaurantId, @Nullable LocalDate date) {
         log.info("getAll");
-        return repository.getAll(restaurantId, ifNullThenNow(date));
+        return repository.getAllByRestaurantAndDate(restaurantId, ifNullThenNow(date));
     }
 
     public Dish get(int id, int restaurantId) {
         log.info("get {}", id);
-        return checkNotFoundWithId(repository.get(id, restaurantId), id);
+        return checkNotFoundWithId(repository.getById(id, restaurantId), id);
     }
 
     @CacheEvict(value = "menu", allEntries = true)
     public void delete(int id, int restaurantId) {
         log.info("delete {}", id);
-        checkNotFoundWithId(repository.delete(id, restaurantId), id);
+        checkNotFoundWithId(repository.delete(id, restaurantId) != 0, id);
     }
 
+    @Transactional
     @CacheEvict(value = "menu", key = "#dish.date")
     public Dish create(Dish dish, int restaurantId) {
         checkNew(dish);
         log.info("create {}", dish);
         Assert.notNull(dish, "dish must not be null");
-        return repository.save(dish, restaurantId);
+        dish.setRestaurant(restaurantRepository.getOne(restaurantId));
+        return repository.save(dish);
     }
 
+    @Transactional
     @CacheEvict(value = "menu", key = "#dish.date")
     public void update(Dish dish, int id, int restaurantId) {
         assureIdConsistent(dish, id);
         log.info("update {}", dish);
         Assert.notNull(dish, "dish must not be null");
-        checkNotFoundWithId(repository.save(dish, restaurantId), dish.id());
+        // check dish belongs to restaurant
+        get(dish.getId(), restaurantId);
+        dish.setRestaurant(restaurantRepository.getOne(restaurantId));
+        checkNotFoundWithId(repository.save(dish), dish.id());
     }
 
     @Cacheable(value = "menu", key = "#date")
     public List<MenuTo> getMenuByDate(@NotNull LocalDate date) {
         log.info("get menu by date {}", date);
-        List<Dish> dishes = repository.getMenuByDate(date);
+        List<Dish> dishes = repository.getAllByDate(date);
         return dishes.stream()
                 .collect(Collectors.groupingBy(Dish::getRestaurant, Collectors.toList()))
                 .entrySet().stream()
